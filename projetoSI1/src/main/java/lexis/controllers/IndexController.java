795675f5415b4fd9190cb2bc8a34cbe8d07a12f0
BrenoSouza <jose.souza@ccc.ufcg.edu.br>
2016-07-27 00:45:55 -0300

    Start!!

---
+++ projetoSI1/src/main/java/lexis/controllers/IndexController.java
@@ -0,0 +1,45 @@
+package lexis.controllers;
+
+import org.h2.engine.SysProperties;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import lexis.domain.Users;
+
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+	
+	@RequestMapping(value = "/{url}")
+    public String index(@PathVariable("url") String url){
+		System.out.println("url: "+url);
+		if(url.equals("index")){
+			return "index";
+		}
+		return "error";
+    }
+	
+	@RequestMapping("/")
+	public String index(){
+		return "index";
+	}
+    
+    @RequestMapping(method = RequestMethod.POST)
+    public String adicionaUsers(String login,String senha,String email, Users user, Model model) {
+          user.setLogin(login);
+          user.setSenha(senha);
+          user.setEmail(email);
+          model.addAttribute("user", user);
+          System.out.println("login"+ user.getLogin());
+          System.out.println("email:" + user.getEmail());
+          System.out.println("senha" + user.getSenha());
+          System.out.println("Id" + user.getId());
+          return "editor";
+    }
+    
+}
