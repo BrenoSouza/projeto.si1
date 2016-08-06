199b5e53a199dd774e5ea37b6a0d11e9fbbf0fb3
BrenoSouza <jose.souza@ccc.ufcg.edu.br>
2016-08-06 13:30:32 -0300

    RegisterController

--- projetoSI1/src/main/resources/static/js/LoginController.js
+++ projetoSI1/src/main/resources/static/js/LoginController.js
@@ -1,9 +1,17 @@
-var appL = angular.module("loginC", []);
+var appL = angular.module("inicial", []);
 
 appL.controller("loginCtrl", function($scope) {
 
-		$scope.login = "";
+		$scope.login = "okk";
 		$scope.email = "";
 		$scope.password = "";
+
+});
+
+appL.controller("RegisterCtrl", function($scope) {
+
+	$scope.login = "";
+	$scope.email = "jose.souza";
+	$scope.password = "";
 
 });
