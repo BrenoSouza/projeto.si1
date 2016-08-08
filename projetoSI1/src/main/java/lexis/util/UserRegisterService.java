173e3b3b508a830e1422694c6c861c3a520c6f40
raimundoheitor <raimundo.miranda@ccc.ufcg.edu.br>
2016-08-07 21:31:43 -0300

    classe especialista no servico de regitração de usuario

--- projetoSI1/src/main/java/lexis/util/UserRegisterService.java
+++ projetoSI1/src/main/java/lexis/util/UserRegisterService.java
@@ -1,76 +1,82 @@
 package lexis.util;
 
-
-
+import lexis.exceptions.*;
 import lexis.models.User;
 import lexis.services.UserService;
 
-public class CheckUserRegister {
+public class UserRegisterService {
 	
 	private UserService userService;
-	private boolean hasError;
-	private String error;
+	private User userTemp;
 	
-	public CheckUserRegister(UserService userService){
+	public UserRegisterService(UserService userService){
 		this.userService = userService;
 	}
 	
-	public boolean hasErrorIn(User user){
+	public void hasErrorIn(User user){
 		check(user);
-		return hasError;
-	}
-	
-	public String getError(){
-		return error;
+		return;
 	}
 
 	private void check(User user) {
 		
-		checksEmailIsUsed(user);
-		
-		checksLoginIsUsed(user);
+		checkLoginIsEmpty(user);
 		
 		checkEmailIsEmpty(user);
 		
-		checkLoginIsEmpty(user);
+		checksLoginIsUsed(user);
+		
+		checksEmailIsUsed(user);
 	}
-	
-	
 	
 	//verifica se o email esta em uso
-	private void checksEmailIsUsed(User user) {
+	private void checksEmailIsUsed(User user) throws EmailIsUsedException{
 		if(userService.existsByEmail(user.getEmail())){
-			setError(true, "email ja em uso");
-			return;
+			throw new EmailIsUsedException("email ja em uso");
 		}
 	}
+	
 	//verifica se o login esta em uso
-	private void checksLoginIsUsed(User user) {
+	private void checksLoginIsUsed(User user) throws LoginIsUsedException{
 		if(userService.existsByLogin(user.getLogin())){
-			setError(true, "login ja em uso");
-			return;
+			throw new LoginIsUsedException("login ja em uso");
 		}
 	}
+	
 	// verifica se o email é vazio
-	private void checkEmailIsEmpty(User user) {
+	public void checkEmailIsEmpty(User user) throws EmailEmptyException{
 		if (user.getEmail().isEmpty()) {
-			setError(true, "Email não pode ser vazio");
-			return;
+			throw new EmailEmptyException("Email não pode ser vazio");
 		}
 	}
+	
 	//verifica se o login é vazio
-	public void checkLoginIsEmpty(User user) {
+	
+	public void checkLoginIsEmpty(User user) throws LoginEmptyException {
 		if(user.getLogin().isEmpty()){
-			setError(true, "Login não pode ser vazio");
-			return;
+			throw new LoginEmptyException("Login não pode ser vazio");
 		}
 	}
 	
-	private void setError(boolean hasError, String erro){
-		this.error = erro;
-		this.hasError = hasError;
+	// verifica se o email esta cadastrado
+	public void checkEmailIsRegistered(User user) throws EmailNotRegisteredException {
+		this.userTemp = userService.getUserByEmail(user.getLogin());
+		if (userTemp == null) {
+			throw new EmailNotRegisteredException("email nao cadastrado");
+		}
 	}
 	
+	// verifica se o login esta cadastrado
+	public void checkLoginIsRegistered(User user) throws LoginNotRegisteredException {
+		userTemp = userService.getUserByLogin(user.getLogin());
+		if (userTemp == null) {
+			throw new LoginNotRegisteredException("usuario nao cadastrado");
+		}
+	}
 	
-
+	public void RegisterUser(User userRegister) {
+		userService.saveUser(userRegister);
+		
+	}
+	
 }
