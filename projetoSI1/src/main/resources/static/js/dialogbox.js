4f519eedce28028c73be8295b5e105e072a41a8c
MelisseCabral <melissecabral07@gmail.com>
2016-09-10 00:56:25 -0300

    Adição de caixas de dialogos custumizadas para alerta e confirmação e adição de menu.

---
+++ projetoSI1/src/main/resources/static/js/dialogbox.js
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+
+function CustomAlert(){
+    this.render = function(dialog){
+        var winW = window.innerWidth;
+        var winH = window.innerHeight;
+        var dialogoverlay = document.getElementById('dialogoverlay');
+        var dialogbox = document.getElementById('dialogbox');
+        dialogoverlay.style.display = "block";
+        dialogoverlay.style.height = winH+"px";
+        dialogbox.style.left = (winW/2) - (550 * .5)+"px";
+        dialogbox.style.top = "100px";
+        dialogbox.style.display = "block";
+        document.getElementById('dialogboxhead').innerHTML ="";
+        document.getElementById('dialogboxbody').innerHTML = dialog;
+        document.getElementById('dialogboxfoot').innerHTML = '<button onclick="Alert.ok()">OK</button>';
+    }
+	this.ok = function(){
+		document.getElementById('dialogbox').style.display = "none";
+		document.getElementById('dialogoverlay').style.display = "none";
+	}
+}
+
+function deleteFile(id){
+	var db_id = id.replace("file_", "");
+	// metodo a ser implementado
+}
+function CustomConfirm(){
+	this.render = function(dialog,op,id){
+		var winW = window.innerWidth;
+	    var winH = window.innerHeight;
+		var dialogoverlay = document.getElementById('dialogoverlay');
+	    var dialogbox = document.getElementById('dialogbox');
+		dialogoverlay.style.display = "block";
+	    dialogoverlay.style.height = winH+"px";
+		dialogbox.style.left = (winW/2) - (550 * .5)+"px";
+	    dialogbox.style.top = "100px";
+	    dialogbox.style.display = "block";
+		
+		document.getElementById('dialogboxhead').innerHTML = "";
+	    document.getElementById('dialogboxbody').innerHTML = dialog;
+		document.getElementById('dialogboxfoot').innerHTML = '<button onclick="Confirm.yes(\''+op+'\',\''+id+'\')">Sim</button> <button onclick="Confirm.no()">Não</button>';
+	}
+	this.no = function(){
+		document.getElementById('dialogbox').style.display = "none";
+		document.getElementById('dialogoverlay').style.display = "none";
+	}
+	this.yes = function(op,id){
+		if(op == "delete_file"){
+			deleteFile(id);
+		}
+		document.getElementById('dialogbox').style.display = "none";
+		document.getElementById('dialogoverlay').style.display = "none";
+	}
+}
+
+var Confirm = new CustomConfirm();
+var Alert = new CustomAlert();
---
+++ projetoSI1/src/main/resources/static/js/menu.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+function clickMenu(){
+	var menu = document.getElementById("menu");
+	if(menu.style.opacity == 1){
+		closeMenu();
+	} else {
+		openMenu();
+	}
+}
+
+function openMenu(){
+	menu.style.left = "0px"; // return it to active screen space
+	menu.style.opacity = 1;
+}
+
+function closeMenu(){
+	menu.style.opacity = 0;
+	menu.style.left = "-260px"; // remove it from active screen space
+}
