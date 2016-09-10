2cdf97803976f890279f34b15b9adc4a783b2995
raimundoheitor <raimundo.miranda@ccc.ufcg.edu.br>
2016-09-10 03:26:42 -0300

    refatoração

--- projetoSI1/src/main/java/lexis/controllers/EditorController.java
+++ projetoSI1/src/main/java/lexis/controllers/EditorController.java
@@ -1,9 +1,5 @@
 package lexis.controllers;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,15 +7,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.fasterxml.jackson.core.JsonGenerationException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import lexis.models.DataBase;
 import lexis.models.Explorer;
 import lexis.models.File;
-import lexis.models.Folder;
 import lexis.models.Type;
 import lexis.models.User;
 import lexis.util.JsonUtil;
@@ -48,8 +38,7 @@ public class EditorController {
 		JsonUtil.json(json);
 		
 		setExplorer();
-		
-		
+
 		String fileName = JsonUtil.getName();
 		Type type = JsonUtil.getType();
 		System.out.println(fileName+"/"+type);
@@ -61,28 +50,15 @@ public class EditorController {
 	public File viewFile() {
 		return file;
 	}
-	
-	
-	private Map<String, Object> jsonToMap(Object json) {
-		ObjectMapper mapper = new ObjectMapper();
-		try {
-			String jsonToString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(json);
+
+	@RequestMapping(value = "saveData", method = RequestMethod.POST)
+	public void saveData(@RequestBody Object json){
+		JsonUtil.json(json);
 		
-			Map<String, Object> map = new HashMap<String, Object>();
-
-			// convert JSON string to Map
-			map = mapper.readValue(jsonToString, new TypeReference<Map<String, String>>(){});
-			return map;
-
-		} catch (JsonGenerationException e) {
-			e.printStackTrace();
-		} catch (JsonMappingException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
+		String data = JsonUtil.getData();
+		file.setData(data );
 	}
+	
 	private User userLogged(){
 		return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 	}
