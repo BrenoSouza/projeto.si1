3835fedd517e7ee8a82b6240a791f9d3733e3c78
BrenoSouza <jose.souza@ccc.ufcg.edu.br>
2016-09-11 04:00:32 -0300

    US - 11: Configurações para o deploy

--- projetoSI1/src/main/resources/static/js/home.js
+++ projetoSI1/src/main/resources/static/js/home.js
@@ -14,7 +14,7 @@ app.controller("listaArquivosCtrl", function($scope, $http) {
     
     
     var loadingFolder = function() {
-    	$http.get("http://localhost:8080/home/explorer").success(function(data, status) {
+    	$http.get("/home/explorer").success(function(data, status) {
     		
     		$scope.arquivos = [];
     		
@@ -52,7 +52,7 @@ app.controller("listaArquivosCtrl", function($scope, $http) {
             	dateCreation: date.toISOString().substring(0, 10) + " " + date.toString().substring(16, 24)
          };
         
-        $http.post('http://localhost:8080/home/newFolder', folder).success(function(data, status) {
+        $http.post('/home/newFolder', folder).success(function(data, status) {
             loadingFolder();
 
         });
@@ -80,7 +80,7 @@ app.controller("listaArquivosCtrl", function($scope, $http) {
         	}
 
         	
-        	$http.post("http://localhost:8080/home/newFile", file).success(function(data, status) {
+        	$http.post("/home/newFile", file).success(function(data, status) {
         		loadingFolder();
         		if ($scope.arquivos.length === 0) {
         			$scope.viewFile(file);
@@ -102,20 +102,20 @@ app.controller("listaArquivosCtrl", function($scope, $http) {
     }
 
     $scope.changeFolder = function(name) {
-        $http.get("http://localhost:8080/home/explorer/" + name).success(function(data, status) {
+        $http.get("/home/explorer/" + name).success(function(data, status) {
 			loadingFolder();
     	});
     }
 
     $scope.backFolder = function() {
-    	$http.get("http://localhost:8080/home/explorer/back").success(function(data, status) {
+    	$http.get("/home/explorer/back").success(function(data, status) {
 			loadingFolder();
     	});
     }
     
     $scope.deleteFolder = function(name) {
     	
-    	$http.post("http://localhost:8080/home/deleteFolder", name).success(function(data, status) {
+    	$http.post("/home/deleteFolder", name).success(function(data, status) {
 			loadingFolder();
     	});
     }
@@ -129,7 +129,7 @@ app.controller("listaArquivosCtrl", function($scope, $http) {
     			newName: nName
     		}
     		
-    		$http.post("http://localhost:8080/home/renameFolder", data).success(function(data, status) {
+    		$http.post("/home/renameFolder", data).success(function(data, status) {
     			loadingFolder();
         	});
     	}
@@ -147,7 +147,7 @@ app.controller("listaArquivosCtrl", function($scope, $http) {
     		type: fileType
     	}
     	
-    	$http.post("http://localhost:8080/home/deleteFile", file).success(function(data, status) {
+    	$http.post("/home/deleteFile", file).success(function(data, status) {
 			loadingFolder();
     	});
     	
@@ -164,7 +164,7 @@ app.controller("listaArquivosCtrl", function($scope, $http) {
     			newType: nType
     		}
     	
-    		$http.post("http://localhost:8080/home/renameFile", data).success(function(data, status) {
+    		$http.post("/home/renameFile", data).success(function(data, status) {
     			loadingFolder();
     		});
     	}
@@ -204,7 +204,7 @@ app.controller("listaArquivosCtrl", function($scope, $http) {
     		
     	}
     	
-    	$http.post("http://localhost:8080/editor/viewFile", file).success(function(data, status) {
+    	$http.post("/editor/viewFile", file).success(function(data, status) {
 
     	});
     	
